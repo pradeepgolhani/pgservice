39f2ef1fe751b146d575288c69a65b6e8f876aa9
Pradeep G <pradeepgolhani@gmail.com>
2018-01-29 02:43:45 +0530

    updated code

--- src/main/java/com/pg/util/AppUtil.java
+++ src/main/java/com/pg/util/AppUtil.java
@@ -4,18 +4,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import org.apache.commons.io.IOUtils;
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
 public class AppUtil {
-	
+	private static final String SCOPES[] = { "https://www.googleapis.com/auth/firebase.messaging" };// ,
+																									// "https://www.googleapis.com/auth/cloud-platform"
+
 	public static String getAccessToken() throws IOException {
-		InputStream inputStream = AppUtil.class.getResourceAsStream("abc.json");
-		GoogleCredential googleCredential = GoogleCredential.fromStream(
-				inputStream
-				).createScoped(
-						Arrays.asList(SCOPES));
-		googleCredential.refreshToken();
+		GoogleCredential googleCredential = null;
+		try {
+			InputStream inputStream = AppUtil.class
+					.getResourceAsStream("abc.json");
+			String result = IOUtils.toString(inputStream, "UTF-8");
+			System.out.println(result);
+			googleCredential = GoogleCredential.fromStream(inputStream)
+					.createScoped(Arrays.asList(SCOPES));
+			googleCredential.refreshToken();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return googleCredential.getAccessToken();
 	}
-	private static final String SCOPES[] = {"https://www.googleapis.com/auth/firebase.messaging"};//, "https://www.googleapis.com/auth/cloud-platform"
+
 }
